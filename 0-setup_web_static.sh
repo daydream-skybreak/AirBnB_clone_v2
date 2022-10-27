74368ceac7bd72b119d857ec7fe5d183cb3141f5
Samuel <samueldesta323@gmail.com>
2022-10-27 20:41:32 +0300

    added file 0:

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# script that sets up webserver for the deployment of web_static
+# Sets up a web server for deployment of web_static.
+
 apt-get update
-apt-get install nginx -y
+apt-get install -y nginx
 
-mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test/
-echo "this is my test file" > /data/web_static/releases/test/index.html
-
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -15,21 +15,23 @@ chgrp -R ubuntu /data/
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
-        return 301 https://cat-bounce.com/;
+        return 301 http://cuberule.com/;
     }
 
     error_page 404 /404.html;
     location /404 {
-        root /var/www/html;
-        internal;
+      root /var/www/html;
+      internal;
     }
 }" > /etc/nginx/sites-available/default
 
